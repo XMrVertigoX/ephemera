a59148894ef5786a6639a25426d73a7bec54032c
c.s.w.friedrich <c.s.w.friedrich@476fbe68-f37f-d372-4ee4-7252b3122af6>
2011-04-11 09:18:45 +0000

    jME-Testklasse hinzugefuegt.

---
+++ trunk/ephemera/src/ephemera/jMETest.java
@@ -0,0 +1,14 @@
+package ephemera;
+
+import com.jme.app.SimpleGame;
+
+public class jMETest extends SimpleGame {
+
+	protected void simpleInitGame() { }
+	
+	public static void main(String[] args) {
+		jMETest t = new jMETest();
+		t.start();
+	}
+
+}
